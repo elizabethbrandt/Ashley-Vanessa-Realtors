dc23c572d3d171c9cabc03d2b1a861202535b6ff
Elizabeth Brandt <ebrandt8@uw.edu>
2021-04-06 23:27:54 -0700

    created very basic routing

--- ashleyvanessarealtors/client/src/App.js
+++ ashleyvanessarealtors/client/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Nav from './components/Nav';
+import Home from './pages/Home';
+import About from './pages/About';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
