9ce1e4344edac465aa1ec3cc6060ad7537fed05b
Elizabeth Brandt <ebrandt8@uw.edu>
2021-04-20 18:48:59 -0700

    edited navbard

--- ashleyvanessarealtors/client/src/components/Nav/index.js
+++ ashleyvanessarealtors/client/src/components/Nav/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import {AppBar, Toolbar, Typography, Button, IconButton} from '@material-ui/core';
+import {AppBar, Toolbar, Typography, Button, IconButton, MenuItem} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles((theme) => ({
@@ -22,13 +22,16 @@ export default function Nav() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
+          </IconButton> */}
+          <MenuItem>Sell</MenuItem>
+          <MenuItem>Buy</MenuItem>
+          <MenuItem>About</MenuItem>
+          <MenuItem>Contact</MenuItem>
+          {/* <Typography variant="h6" className={classes.title}>
             Ashley Vanessa Realtors
-          </Typography>
-          <Button color="inherit">Contact</Button>
+          </Typography> */}
         </Toolbar>
       </AppBar>
     </div>
